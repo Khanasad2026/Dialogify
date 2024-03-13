67b43544eed8b630f3db26069a978b74ad0cc157
Khanasad <leemaj1954@gamil.com>
2024-03-13 16:56:12 +0530

    ok

--- initial/server/index.js
+++ initial/server/index.js
@@ -5,7 +5,9 @@ import AuthRoutes from "./routes/AuthRoutes.js";
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth",AuthRoutes)
